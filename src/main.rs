ace75ef9d60cd3931665c694ee9efc9977b9cf6a
dhruv6397 <vermadhruv6397@gmail.com>
2024-09-13 15:29:38 +0530

    adding content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let greeting:String = String::from("hello boss");
+    println!("{}",greeting);
+    println!("{}",greeting.chars().nth(1).unwrap());
+
+    let is_even: bool = true;
+    if is_even {
+        println!("the number is even");
+    }
+    for i in 0..11{
+        print!("{} ",i);
+    }
+    println!();
+    let first_word = get_first_word(greeting);
+    print!("{}",first_word);
+}
+
+
+fn get_first_word(sentence:String)->String{
+    let mut ans:String = String::from("");
+    for char in sentence.chars(){
+        ans.push_str(char.to_string().as_str());
+        if char ==' '{
+            break;
+        }
+    }
+    return ans;
 }
